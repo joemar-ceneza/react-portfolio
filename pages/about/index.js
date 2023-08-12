fd8ea93524cefdb1673e9697d539ee6f2f4b03b8
joemar-ceneza <joemar.ceneza@gmail.com>
2023-08-13 01:48:57 +0800

    Trial mode

--- pages/about/index.js
+++ pages/about/index.js
@@ -196,7 +196,7 @@ export default function About() {
           className="flex flex-col w-full xl:max-w-[48%] h-[480px]"
         >
           <div className="flex gap-x-4 xl:gap-x-8 mx-auto xl:mx-0 mb-4">
-            {aboutData.map((item, itemIndex) => {
+            {/* {aboutData.map((item, itemIndex) => {
               return (
                 <div
                   key={itemIndex}
@@ -209,21 +209,19 @@ export default function About() {
                   {item.title}
                 </div>
               );
-            })}
+            })} */}
           </div>
           <div className="py-2 xl:py-6 flex flex-col gap-y-2 xl:gap-y-4 items-center xl:items-start">
-            {aboutData[index].info.map((item, itemIndex) => {
+            {/* {aboutData[index].info.map((item, itemIndex) => {
               return (
                 <div
                   key={itemIndex}
                   className="flex-1 flex flex-col md:flex-row max-w-max gap-x-2 items-center text-white/60"
                 >
-                  {/* title */}
                   <div className="font-light mb-2 md:mb-0">{item.title}</div>
                   <div className="hidden md:flex">-</div>
                   <div>{item.stage}</div>
                   <div className="flex gap-x-4">
-                    {/* icons */}
                     {item.icons?.map((icon, iconIndex) => {
                       return (
                         <div key={iconIndex} className="text-2xl">
@@ -234,7 +232,7 @@ export default function About() {
                   </div>
                 </div>
               );
-            })}
+            })} */}
           </div>
         </motion.div>
       </div>

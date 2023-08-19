d3983d7f055cc76f2bf8c209bc041cabd757cc64
joemar-ceneza <joemar.ceneza@gmail.com>
2023-08-19 11:06:18 +0800

    Fixing errors in contact form

--- pages/api/contact.js
+++ pages/api/contact.js
@@ -10,11 +10,13 @@ export default async function handler(req, res) {
       await transporter.sendMail({
         ...mailOptions,
         subject: data.subject,
+        text: "This is a test string",
         html: `<p>Name: ${data.name}</p><p>Email: ${data.email}</p><p>Message: ${data.message}</p>`,
       });
+      return res.status(200).json({ sucess: true });
     } catch (error) {
       console.log(error);
-      res.status(400).json({ message: error.message });
+      return res.status(400).json({ message: error.message });
     }
   }
   res.status(400).json({ message: "Bad request" });

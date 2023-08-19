5d96286e9542a63b5cca2aceb1c88cd402339490
joemar-ceneza <joemar.ceneza@gmail.com>
2023-08-19 11:28:00 +0800

    Adding a header and icon to the website

--- pages/_app.js
+++ pages/_app.js
@@ -10,10 +10,17 @@ import { useRouter } from "next/router";
 // framer motion
 import { AnimatePresence, motion } from "framer-motion";
 
+// head
+import Head from "next/head";
+
 export default function MyApp({ Component, pageProps }) {
   const router = useRouter();
   return (
     <Layout>
+      <Head>
+        <title>Joemar Ceneza</title>
+        <link rel="icon" type="image/png" href="/icon.png" />
+      </Head>
       <AnimatePresence mode="wait">
         <motion.div key={router.route} className="h-full">
           <Transition />
